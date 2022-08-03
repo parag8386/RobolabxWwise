62dabd6f86b27c3806a0cda5cf5c584ff1891501
paragoswal <paragoswal@gmail.com>
2022-08-03 14:09:38 +0530

    Mac settings

--- Robolab_Code/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Robolab_Code/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,14 +13,14 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_ALARM_01 = 832851525U;
-        static const AkUniqueID PLAY_ALARM_02 = 832851526U;
-        static const AkUniqueID PLAY_ALARM_03 = 832851527U;
-        static const AkUniqueID PLAY_AMBIENCE = 278617630U;
-        static const AkUniqueID PLAY_FAN = 2757069037U;
-        static const AkUniqueID PLAY_FAN_BLADE = 4112445110U;
-        static const AkUniqueID PLAY_GLASSPAD = 4284053745U;
-        static const AkUniqueID PLAY_INDOORLAB = 245143100U;
+        static const AkUniqueID PLAY_ALARM_01_LOOP = 827339530U;
+        static const AkUniqueID PLAY_ALARM_02_LOOP = 726066639U;
+        static const AkUniqueID PLAY_ALARM_03_LOOP = 2797744060U;
+        static const AkUniqueID PLAY_AMBIENCE_LOOP = 1026169559U;
+        static const AkUniqueID PLAY_FAN_BLADE_LOOP = 891405407U;
+        static const AkUniqueID PLAY_FAN_LOOP = 1297944866U;
+        static const AkUniqueID PLAY_GLASSPAD_LOOP = 166761806U;
+        static const AkUniqueID PLAY_INDOORLAB_LOOP = 791875877U;
         static const AkUniqueID PLAY_LIGHTS_FLICKER = 384671264U;
         static const AkUniqueID PLAY_LIGHTS_ON = 406918311U;
         static const AkUniqueID PLAY_MUSIC_LOOP = 433710372U;
@@ -30,7 +30,7 @@ namespace AK
         static const AkUniqueID PLAY_POWERUP = 2876792902U;
         static const AkUniqueID PLAY_RADIO_OFF = 1863306705U;
         static const AkUniqueID PLAY_RADIO_ON = 2948213189U;
-        static const AkUniqueID PLAY_ROBOT_ARM_MOTOR = 2931782723U;
+        static const AkUniqueID PLAY_ROBOT_ARM_MOTOR_LOOP = 172772784U;
         static const AkUniqueID PLAY_ROBOT_BATTERING_RAM = 1204677452U;
         static const AkUniqueID PLAY_STATIC = 423060956U;
         static const AkUniqueID PLAY_TERMINALS = 1220490213U;
@@ -44,11 +44,18 @@ namespace AK
         static const AkUniqueID PLAY_VO_F_LINE06 = 4240237451U;
         static const AkUniqueID PLAY_VO_F_LINE07 = 4240237450U;
         static const AkUniqueID PLAY_VO_F_LINE08 = 4240237445U;
-        static const AkUniqueID STOP_ALARM_01 = 186661467U;
-        static const AkUniqueID STOP_ALARM_02 = 186661464U;
-        static const AkUniqueID STOP_ALARM_03 = 186661465U;
+        static const AkUniqueID STOP_ALARM_01_LOOP = 1249145528U;
+        static const AkUniqueID STOP_ALARM_02_LOOP = 3095460281U;
+        static const AkUniqueID STOP_ALARM_03_LOOP = 436683974U;
         static const AkUniqueID STOP_ALL = 452547817U;
+        static const AkUniqueID STOP_AMBIENCE_LOOP = 116129673U;
+        static const AkUniqueID STOP_FAN_BLADE_LOOP = 1552245617U;
+        static const AkUniqueID STOP_FAN_LOOP = 2547894692U;
+        static const AkUniqueID STOP_GLASSPAD_LOOP = 4172250560U;
+        static const AkUniqueID STOP_INDOORLAB_LOOP = 2656924299U;
         static const AkUniqueID STOP_MUSIC_LOOP = 3130332582U;
+        static const AkUniqueID STOP_ROBOT_ARM_MOTOR_LOOP = 1332730642U;
+        static const AkUniqueID STOP_TURBINE_LOOP = 2875175866U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
@@ -76,6 +83,7 @@ namespace AK
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMBIENCE = 85412153U;
         static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID SFX = 393239870U;
         static const AkUniqueID VO = 1534528548U;
@@ -83,6 +91,7 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID AMBIENCE = 85412153U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID SFX = 393239870U;
